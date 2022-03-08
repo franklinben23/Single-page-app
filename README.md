99a1423d37d44ddc984264682b4697f7c12f19b0
Emperor_charly <charleschukwunweikeeneh@gmail.com>
2022-03-08 20:56:20 +0100

    Resolve webpack errors

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# js-capstone
-In this repository we will create a single page web application that interacts with an external API
+# webpack-demo
--- webpack.config.js
+++ webpack.config.js
@@ -3,27 +3,34 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  devtool: 'inline-source-map',
+
   entry: './src/index.js',
+   
   devServer: {
     static: './dist',
   },
+
+   plugins: [
+      new HtmlWebpackPlugin({
+      template: './src/index.html'
+      }),
+   ],
+   
+   output: {
+     filename: 'bundle.js',
+     path: path.resolve(__dirname, 'dist'),
+   },
+   
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'src/index1.html',
-    }),
-  ],
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-};
+ };
