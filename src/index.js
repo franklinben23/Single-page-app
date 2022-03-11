e47048db33f24fe600c9fedadce32e1a7504f396
Emperor_charly <charleschukwunweikeeneh@gmail.com>
2022-03-11 13:40:18 +0100

    More Styling of the popup page

--- src/index.js
+++ src/index.js
@@ -5,9 +5,4 @@ import renderCards from './modules/renderContent.js';
 const image = document.querySelector('.logo');
 image.innerHTML = `<img class="img-logo" src="${img}" alt="my logo">`;
 
-// const displayModal = document.querySelector('.modal-content');
-// const gridContainer = document.querySelector('.grid-container');
-// const header = document.querySelector('.header');
-// const footer = document.querySelector('.footer');
-
 renderCards();
